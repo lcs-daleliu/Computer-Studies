ef91812d36611d9e0612fb52755a1639d2e15c78
Dale Liu <YLiu@lcs.on.ca>
2024-01-15 15:23:32 -0500

    Created the initial static layout.

---
+++ StepperExample/StepperExample/StepperExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepperExampleApp.swift
+//  StepperExample
+//
+//  Created by Yiren LIU on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct StepperExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
